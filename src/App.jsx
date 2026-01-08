f3c6a24422524518596cbe5d0a6b69455285ce85
bhavyacoder2008 <bhavyamanochaviiilily@gmail.com>
2026-01-08 23:52:49 +0530

    Fix Vite base path for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,12 @@ function App() {
   const winningPositions = [[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,4,6],[2,5,8],[3,4,5],[6,7,8]];
 
 
+  function firse(){
+    setArray(Array(9).fill(null))
+    setwinner(null)
+    setCurrentuser("X")
+  }
+
 
   function CheckWinner(newarry){
     for(let pos of winningPositions){
@@ -51,7 +57,7 @@ function App() {
       <Header />
       <Box array = {array} fun={Clickwala}/>
       {
-        winner ? <p> Winner is : {winner}</p> : <p>Next Move : {currentuser}</p> // winner ki value truthy tabhi hogi jab abc truthy hoga and wo truthy tabhi hoga jab Checkwinner(newarr) ek truthy valuye return karega and wo tabhi return karega jab game jeetenge
+        winner ? <p> Winner is : {winner} <br /><button onClick={firse}>Restart</button></p> : <p>Next Move : {currentuser}</p> // winner ki value truthy tabhi hogi jab abc truthy hoga and wo truthy tabhi hoga jab Checkwinner(newarr) ek truthy valuye return karega and wo tabhi return karega jab game jeetenge
       }
     </>
   )
